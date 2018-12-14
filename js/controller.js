26c976809f765b245755d2579c681d2c6aba864c
rsidhu07 <rajsidhuh07@gmail.com>
2018-12-14 17:20:15 +0530

    added comments

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,10 @@
 app.controller("myctrl",function($scope){
 
-    $scope.fnum= 0;
+    $scope.fnum= 0; //set initial value of text box to '0'
     $scope.snum= 0;
 
     $scope.result = 0;
-/*
+/* Alternate logic for calculator
     $scope.result = function() {
         if ($scope.operator == '+') {
             return $scope.fnum + $scope.snum;
@@ -24,14 +24,14 @@ app.controller("myctrl",function($scope){
     $scope.result = ()=>{
 
         switch ($scope.operator) {
-            case '+': return $scope.fnum + $scope.snum;
+            case '+': return $scope.fnum + $scope.snum; //will return sum if "+" operator is selected
                
-            case '-': return $scope.fnum - $scope.snum;
+            case '-': return $scope.fnum - $scope.snum; //will return difference if "-" operator is selected
         
                   
-            case '*': return $scope.fnum * $scope.snum;
+            case '*': return $scope.fnum * $scope.snum; //will return multiplication of two numbers if "*" operator is selected
                     
-            case '/': return $scope.fnum / $scope.snum;
+            case '/': return $scope.fnum / $scope.snum; //will return division of two numbers if "/" operator is selected
                 
         
             }
